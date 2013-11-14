36168096fe1cd727849bcb501e64e3edde7cb53f
Daniel Hilst Selli <danielhilst@gmail.com>
2013-11-14 02:22:37 +0000

    ..

--- pastebin-2.0.el
+++ pastebin-2.0.el
@@ -24,14 +24,11 @@
   "a paste")
 
 (defclass paste-user ()
-  ((dev-api-key :initarg :dev-api-key "Your developer key from http://pastebin.com/api")
-   (dev-usr-key :initarg :dev-usr-key "Your user key, retrived from pastebin")
+  ((dev-key :initarg :dev-key "Your developer key from http://pastebin.com/api")
+   (usr-key :initarg :usr-key "Your user key, retrived from pastebin")
    (password :initarg :password "Your password, clear text honey!!")
    (username :initarg :username "Your username")))
 
-(defun paste-retriever-helper (url)
-  "Retrieve paste on url on a buffer")
-                
 (defcustom pastebin-post-request-login-url "http://pastebin.com/api/api_login.php"
   "Login url"
   :type 'string
@@ -41,6 +38,61 @@
   "Paste url"
   :type 'string
   :group 'pastebin)
+
+(defun strip-CRs (&optional buffer)
+  "Get rid of damn ^Ms"
+  (let ((buffer (or buffer (current-buffer))))
+    (with-current-buffer buffer
+      (goto-char (point-min))
+      (while (re-search-forward "\r\n" nil t)
+        (replace-match ""))
+      buffer)))
+
+(defun strip-http-header (&optional buffer)
+  "Given a buffer with an HTTP response, remove the header and return the buffer
+If no buffer is given current buffer is used"
+  (let ((buffer (or buffer (current-buffer))))
+    (with-current-buffer buffer
+      (goto-char (point-min))
+      (re-search-forward "\n\n")
+    (kill-region (point-min) (point))
+    buffer)))
+
+(defun paste-retrieve-contents-by-key-h (key)
+  (unless (stringp key)
+    (signal 'wrong-type-argument "`key' should be a string"))
+  (with-current-buffer (url-retrieve-synchronously (concat "http://pastebin.com/raw.php?i=" key))
+    (strip-http-header)))
+
+(defun paste-retrieve-paste-list-xml-h (user &optional count)
+  "Retrieve a list of count (100 by default) pastes from pastebin.com"
+  (unless (paste-user-p user)
+    (signal 'wrong-type-argument "`user' should be a `paste-user'"))
+  (let* ((params (concat "api_dev_key=" (oref user dev-key)
+                         "&api_user_key=" (oref user usr-key)
+                         "&api_results_limits=" (or count (format "%s" 100))
+                         "&api_option=" "list"))
+         (url-request-method "POST")
+         (url-request-extra-headers '(("Content-Type" . "application/x-www-form-urlencoded")))
+         (url-request-data params))
+    (with-current-buffer (url-retrieve-synchronously pastebin-post-request-paste-url)
+      (strip-http-header)
+      (strip-CRs)
+      (current-buffer)
+      )))
+
+(setq me (paste-user "Me" :dev-key pastebin-unique-developer-api-key
+                          :usr-key pastebin-user-api-key
+                          :password pastebin-password
+                          :username pastebin-user-name))
+
+
+
+
+ (with-current-buffer (paste-retrieve-paste-list-xml-h me)
+   (goto-char (point-min))
+   (re-search-forward "</paste>")
+   (xml-parse-region (point-min) (point)))
 
 
 
