e87c1bfd6718e8e37917faf1d2412934f744a685
Daniel Hilst Selli <danielhilst@gmail.com>
2013-11-15 18:37:17 +0000

    refresh-paste-list fixed

--- pastebin-2.0.el
+++ pastebin-2.0.el
@@ -3,6 +3,8 @@
   :tag "Pastebin"
   :group 'tools)
 
+;; PASTE CLASS
+
 (defclass paste ()
   ((key :initarg :key)
    (date :initarg :date)
@@ -16,32 +18,7 @@
    (buffer :initarg :buffer)
    (last-fetched :initarg :last-fetched)
    (user :initarg :user)
-   (hits :initarg :hits))
-  "a paste")
-
-(defclass paste-user ()
-  ((dev-key :initarg :dev-key "Your developer key from http://pastebin.com/api")
-   (usr-key :initarg :usr-key "Your user key, retrived from pastebin")
-   (password :initarg :password "Your password, clear text honey!!")
-   (username :initarg :username "Your username")
-   (paste-list :initarg :paste-list "The list of pastes for this user")
-   (list-buffer :initarg :list-buffer "Done by do-list-buffer")
-  ))
-
-(defmethod login ((user paste-user))
-  "Given user and password login and sets dev-key"
-  (if (slot-boundp user :usr-key)
-      (oref user :usr-key)
-    (let* ((params (concat "api_dev_key=" (oref user :dev-key)
-                           "&api_user_name=" (url-hexify-string (oref user :username))
-                           "&api_user_password=" (url-hexify-string (oref user :password))))
-           (url-request-method "POST")
-           (url-request-extra-headers
-            '(("Content-Type" . "application/x-www-form-urlencoded")))
-           (url-request-data params))
-      (with-current-buffer (url-retrieve-synchronously pastebin-post-request-login-url)
-        (strip-http-header)
-        (oset user :usr-key (buffer-substring-no-properties (point-min) (point-max)))))))
+   (hits :initarg :hits)))
 
 
 (defmethod assign-buffer ((p paste) buf)
@@ -76,6 +53,133 @@
       (current-buffer))
     ))
 
+;; PASTE-USER class
+
+(defclass paste-user ()
+  ((dev-key :initarg :dev-key "Your developer key from http://pastebin.com/api")
+   (usr-key :initarg :usr-key "Your user key, retrived from pastebin")
+   (password :initarg :password "Your password, clear text honey!!")
+   (username :initarg :username "Your username")
+   (paste-list :initarg :paste-list "The list of pastes for this user")
+   (buffer-hash :initarg :list-buffer "Done by do-list-buffer")
+  ))
+
+(defmethod fetch-list-xml ((user paste-user) &optional count)
+  "Fetch the list of pastes as xml, and return that buffer"
+  (let* ((params (concat "api_dev_key=" (oref user dev-key)
+                         "&api_user_key=" (oref user usr-key)
+                         "&api_results_limits=" (format "%d" (or count 100))
+                         "&api_option=" "list"))
+         (url-request-method "POST")
+         (url-request-extra-headers '(("Content-Type" . "application/x-www-form-urlencoded")))
+         (url-request-data params))
+    (with-current-buffer (url-retrieve-synchronously pastebin-post-request-paste-url)
+      (strip-http-header)
+      (strip-CRs)
+      (goto-char (point-max))
+      (current-buffer)
+      )))
+
+(defmethod refresh-paste-list ((user paste-user) &optional count)
+  "Set/Refresh paste-list attr to the list of paste objects retrieved from pastebin.com"
+  (with-current-buffer (fetch-list-xml user (or count pastebin-default-paste-list-limit))
+    (goto-char (point-min))
+    (let ((i (point-min))
+          plist)
+      (while (re-search-forward "</paste>" nil t)
+        (let ((paste-sexp (xml-parse-region i (point))))
+          (setq i (point))
+          (push (paste (concat "paste@" (pastebin-paste-sexp-get-attr-h paste-sexp 'paste_key))
+                               :key (pastebin-paste-sexp-get-attr-h paste-sexp 'paste_key)
+                               :date (pastebin-paste-sexp-get-attr-h paste-sexp 'paste_date)
+                               :title (pastebin-paste-sexp-get-attr-h paste-sexp 'paste_title)
+                               :size (pastebin-paste-sexp-get-attr-h paste-sexp 'paste_size)
+                               :expire_date (pastebin-paste-sexp-get-attr-h paste-sexp 'paste_expire_date)
+                               :private (pastebin-paste-sexp-get-attr-h paste-sexp 'paste_private)
+                               :format_long (pastebin-paste-sexp-get-attr-h paste-sexp 'paste_format_long)
+                               :format_short (pastebin-paste-sexp-get-attr-h paste-sexp 'paste_format_short)
+                               :url (pastebin-paste-sexp-get-attr-h paste-sexp 'paste_url)
+                               :last-fetched (pastebin-paste-sexp-get-attr-h paste-sexp 'paste_last-fetched)
+                               :user user) plist)
+          )
+        )
+      plist
+      )
+    )
+  )
+
+
+;; (setq paste-sexp '((paste nil (paste_key nil A96vA23q) (paste_date nil 1384544651) (paste_title nil *scratch*) (paste_size nil 191) (paste_expire_date nil 0) (paste_private nil 1) (paste_format_long nil None) (paste_format_short nil text) (paste_url nil http://pastebin.com/A96vA23q) (paste_hits nil 0))))
+
+
+(defmethod do-list-buffer ((user paste-user))
+  "Create a buffer with a list of pastes and return it
+Some keybinds are setted"
+  (interactive)
+
+  (login user)
+  
+  (oset user :list-buffer 
+        (get-buffer-create (format "*Pastebin %s List*" (oref user :username))))
+  
+  ;; fetch pastes list
+  (refresh-paste-list user)
+
+  (with-current-buffer (get-buffer (oref user :list-buffer))
+
+    (let ((inhibit-read-only t))
+      (erase-buffer))
+
+    (widget-minor-mode 1)
+    (use-local-map pastebin-list-map)
+    
+    (setq pastebin-list-buffer-user user)
+
+    (widget-insert (format "%5.5s | %-8.8s | %-32.32s | %-7.7s | %-30.30s\n"
+                           "VIEW" "ID" "TITLE" "SYNTAX" "DATE"))
+    (dolist (paste (oref user :paste-list))
+      (widget-create 'link 
+                     :notify (lambda (wid &rest ignore)
+                               (pastebin-fetch-paste-at-point))
+                     :paste paste
+                     :follow-link t
+                     :value (format "%4.4s | %-8.8s | %-32.32s | %-7.7s | %-20.20s"
+                                    (if (string= (oref paste :private) "1")
+                                        "PRIV"
+                                      "PUBL")
+                                    (oref paste :key)
+                                    (or (oref paste :title) "")
+                                    (oref paste :format_short)
+                                    (format-time-string "%c" (seconds-to-time (string-to-number (oref paste :date))))
+                                    )
+                     )
+
+      (widget-insert "\n")
+      )
+    (widget-setup)
+    (goto-char (point-min))
+    (current-buffer)
+    )
+  )
+
+(defmethod login ((user paste-user))
+  "Given user and password login and sets dev-key"
+  (if (slot-boundp user :usr-key)
+      (oref user :usr-key)
+    (let* ((params (concat "api_dev_key=" (oref user :dev-key)
+                           "&api_user_name=" (url-hexify-string (oref user :username))
+                           "&api_user_password=" (url-hexify-string (oref user :password))))
+           (url-request-method "POST")
+           (url-request-extra-headers
+            '(("Content-Type" . "application/x-www-form-urlencoded")))
+           (url-request-data params))
+      (with-current-buffer (url-retrieve-synchronously pastebin-post-request-login-url)
+        (strip-http-header)
+        (oset user :usr-key (buffer-substring-no-properties (point-min) (point-max)))))))
+
+
+
+
 ;; TODO
 (defmethod paste-new ((user paste-user) &optional &key title syntax private buffer)
   "Upload a new paste to pastebin.com
@@ -108,59 +212,13 @@ Set the pastebin-minor-mode on current buffer"
     (switch-to-buffer (current-buffer))))
     
 
-(defmethod fetch-list-xml ((user paste-user) &optional count)
-  (let* ((params (concat "api_dev_key=" (oref user dev-key)
-                         "&api_user_key=" (oref user usr-key)
-                         "&api_results_limits=" (format "%d" (or count 100))
-                         "&api_option=" "list"))
-         (url-request-method "POST")
-         (url-request-extra-headers '(("Content-Type" . "application/x-www-form-urlencoded")))
-         (url-request-data params))
-    (with-current-buffer (url-retrieve-synchronously pastebin-post-request-paste-url)
-      (strip-http-header)
-      (strip-CRs)
-      (goto-char (point-max))
-      (insert params)
-      (current-buffer)
-      )))
 
-(defmethod refresh-paste-list ((user paste-user) &optional count)
-  "Set paste-list attr to the list of paste objects retrieved from pastebin.com"
-  (with-current-buffer (fetch-list-xml me count)
-    (goto-char (point-min))
-    (or (let ((i (point-min))
-              plist)
-          (while (re-search-forward "</paste>" nil t)
-            (let ((p (xml-parse-region i (point))))
-              (setq plist (cons (paste (concat "paste@" (pastebin-paste-sexp-get-attr-h p 'paste_key))
-                                       :key (pastebin-paste-sexp-get-attr-h p 'paste_key)
-                                       :title (pastebin-paste-sexp-get-attr-h p 'paste_title)
-                                       :date (pastebin-paste-sexp-get-attr-h p 'paste_date)
-                                       :size (pastebin-paste-sexp-get-attr-h p 'paste_size)
-                                       :expire_date (pastebin-paste-sexp-get-attr-h p 'paste_expire_date)
-                                       :private (pastebin-paste-sexp-get-attr-h p 'paste_private)
-                                       :format_long (pastebin-paste-sexp-get-attr-h p 'paste_format_long)
-                                       :format_short (pastebin-paste-sexp-get-attr-h p 'paste_format_short)
-                                       :url (pastebin-paste-sexp-get-attr-h p 'paste_url)
-                                       :buffer (current-buffer)
-                                       :last-fetched (float-time)
-                                       :user user
-                                       :hits (pastebin-paste-sexp-get-attr-h p 'paste_hits)) plist))
-              (setq i (point))))
-          (oset user :paste-list (sort plist (lambda (p1 p2)
-                                                (not (string< (oref p1 :date)
-                                                              (oref p2 :date)))))))
 
-        (error "Cant parse any paste"))))
 
-(defun pastebin-paste-sexp-get-attr-h (paste-sexp attr)
-  "Return the attribute `attr' from `paste'
-Attributes are described here: http://pastebin.com/api#9
-`attr' is a symbol
-Ex: (pastebin-paste-get-attr (pastebin-pastes-nth 0) 'paste_tittle)"
-  (unless (symbolp attr)
-    (signal 'wrong-type-argument ("`attr' should be a symbol")))
-  (car (last (assoc attr (nthcdr 2 (car paste-sexp))))))
+
+
+
+;; Local variables and customs
 
 (defcustom pastebin-type-assoc
   '((actionscript-mode . " actionscript")
@@ -243,6 +301,12 @@ Ex: (pastebin-paste-get-attr (pastebin-pastes-nth 0) 'paste_tittle)"
   :type '(alist :key-type symbol :value-tupe string)
   :group 'pastebin)
 
+(defvar pastebin-default-paste-list-limit 100
+  "The number of pastes to retrieve by default")
+
+(defvar pastebin-default-user nil
+  "The default user begin used")
+
 (defvar pastebin-local-buffer-paste nil
   "Every pastebin buffer has a paste object associated with it")
 (make-variable-buffer-local 'pastebin-local-buffer-paste)
@@ -269,6 +333,34 @@ Ex: (pastebin-paste-get-attr (pastebin-pastes-nth 0) 'paste_tittle)"
   :type 'cons
   :group 'pastebin)
 
+(defvar pastebin-list-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "d") 'pastebin-delete-paste-at-point) ;; TODO 
+    (define-key map (kbd "r") 'pastebin-list-buffer-refresh) ;; TODO refresh pastebin list buffer
+    map)
+  "Key map for pastebin list buffer")
+
+;; Local functions and helpers
+
+(defun pastebin-paste-sexp-get-attr-h (paste-sexp attr)
+  "Return the attribute `attr' from `paste'
+Attributes are described here: http://pastebin.com/api#9
+`attr' is a symbol
+Ex: (pastebin-paste-get-attr (pastebin-pastes-nth 0) 'paste_tittle)"
+  (unless (symbolp attr)
+    (signal 'wrong-type-argument ("`attr' should be a symbol")))
+  (car (last (assoc attr (nthcdr 2 (car paste-sexp))))))
+
+(cl-defun pastebin-do-login (&key username password dev-key)
+  "Interface layer, do the login and set `pastebin-default-user'"
+  (unless (and username password dev-key)
+    (error "pastebin-login argument missing"))
+  
+  (setq pastebin-default-user (paste-user username :username username
+                                          :password password
+                                          :dev-key dev-key))
+  (login pastebin-default-user))
+
 (defun strip-CRs (&optional buffer)
   "Get rid of damn ^Ms"
   (let ((buffer (or buffer (current-buffer))))
@@ -288,19 +380,6 @@ If no buffer is given current buffer is used"
     (kill-region (point-min) (point))
     buffer)))
 
-(defvar pastebin-list-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "d") 'pastebin-delete-paste-at-point) ;; TODO 
-    (define-key map (kbd "r") 'pastebin-list-buffer-refresh) ;; TODO refresh pastebin list buffer
-    map)p
-  "Key map for pastebin list buffer")
-
-(defun pastebin-list-buffer-refresh ()
-  "Refresh the list buffer screen
-Operates on current buffer"
-  (interactive)
-  (do-list-buffer pastebin-list-buffer-user))
-  
 (defun pastebin-get-paste-at-point ()
   "Get the paste at point at current-buffer"
   (let ((wid (widget-at)))
@@ -308,77 +387,39 @@ Operates on current buffer"
         (error "No paste at point")
       (widget-get wid :paste))))
 
+(defun pastebin-fetch-paste-at-point ()
+  "Fetch and switch to paste at point"
+  (let ((paste (pastebin-get-paste-at-point)))
+    (fetch-and-process paste)))
+
+;; User interface 
+
+;; FIXME: Error when call from pastebin-delete-paste-at-point
+(defun pastebin-list-buffer-refresh ()
+  "Refresh the list buffer screen
+Operates on current buffer"
+  (interactive)
+  (refresh-paste-list pastebin-default-user)
+  (switch-to-buffer (do-list-buffer pastebin-default-user)))
+  
+;; FIXME
 (defun pastebin-delete-paste-at-point ()
   "Delete the paste at point"
   (interactive)
   (let ((paste (pastebin-get-paste-at-point)))
     (paste-delete paste)
-    (pastebin-list-buffer-refresh))
+    (refresh-paste-list pastebin-default-user 100)
+    (do-list-buffer pastebin-default-user)))
 
-(defun pastebin-fetch-paste-at-point ()
-  "Fetch and switch to paste at point"
-  (let ((paste (pastebin-get-paste-at-point)))
-    (fetch-and-process paste)))
-    
-;; TODO: Implement dired here
-(defmethod do-list-buffer ((user paste-user))
-  "Create a buffer with a list of pastes and switch to it
-Some keybinds are setted"
+
+(defun pastebin-new ()
+  "Create a new paste from buffer"
   (interactive)
+  (paste-new pastebin-default-user))
 
-  (login user)
-  
-  (oset user :list-buffer 
-        (get-buffer-create (format "*Pastebin %s List*" (oref user :username))))
-  
-  ;; fetch pastes list
-  (refresh-paste-list user)
+(pastebin-do-login :dev-key pastebin-unique-developer-api-key
+                   :password pastebin-password
+                   :username pastebin-user-name)
 
-  (with-current-buffer (get-buffer (oref user :list-buffer))
-
-    (let ((inhibit-read-only t))
-      (erase-buffer))
-
-    (widget-minor-mode 1)
-    (use-local-map pastebin-list-map)
-    
-    (setq pastebin-list-buffer-user user)
-
-    (widget-insert (format "%5.5s | %-8.8s | %-32.32s | %-7.7s | %-30.30s\n"
-                           "VIEW" "ID" "TITLE" "SYNTAX" "DATE"))
-    (dolist (paste (oref user :paste-list))
-      (widget-create 'link 
-                     :notify (lambda (wid &rest ignore)
-                               (pastebin-fetch-paste-at-point))
-                     :paste paste
-                     :follow-link t
-                     :value (format "%4.4s | %-8.8s | %-32.32s | %-7.7s | %-20.20s"
-                                    (if (string= (oref paste :private) "1")
-                                        "PRIV"
-                                      "PUBL")
-                                    (oref paste :key)
-                                    (or (oref paste :title) "")
-                                    (oref paste :format_short)
-                                    (format-time-string "%c" (seconds-to-time (string-to-number (oref paste :date))))
-                                    )
-                     )
-
-      (widget-insert "\n")
-      )
-    (widget-setup)
-    (goto-char (point-min))
-    (switch-to-buffer (current-buffer))
-    )
-  )
-
-(setq me (paste-user "Me" :dev-key pastebin-unique-developer-api-key
-                          :password pastebin-password
-                          :username pastebin-user-name))
-
-
-(login me)
-(do-list-buffer me)
-
-(paste-new me)
 
 (provide 'pastebin-2.0)
